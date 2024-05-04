4f8a0ccabd08f7be302481f672957a50813c1f69
RiseCode66 <124663679+RiseCode66@users.noreply.github.com>
2024-05-04 23:50:20 +0200

    commit

--- module/connect.php
+++ module/connect.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-Flight::register('database_connector', 'PDO', array('mysql:host=185.161.10.160;dbname=superdevy_power_switch','root','ggpunish*xd'), function($database_connector) {
-=======
 Flight::register('database_connector', 'PDO', array('mysql:host=185.161.10.160;dbname=superdevy_power_switch','superdevy_root','ggpunish*xd'), function($database_connector) {
->>>>>>> back
     $database_connector->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 });
 
