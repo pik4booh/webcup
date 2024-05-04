006e7b3435bda7a972eab543fad1ceb42d05a9d8
pik4booh <prncrobins2003@gmail.com>
2024-05-04 22:43:46 +0300

    config database

--- module/connect.php
+++ module/connect.php
@@ -1,5 +1,5 @@
 <?php
-Flight::register('database_connector', 'PDO', array('mysql:host=localhost;dbname=superdevy_power_switch','root',''), function($database_connector) {
+Flight::register('database_connector', 'PDO', array('mysql:host=185.161.10.160;dbname=superdevy_power_switch','superdevy_root','ggpunish*xd'), function($database_connector) {
     $database_connector->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 });
 
